d8ceadbc24c0d7bcc2b65e93def86c6bfac84195
cnitishkumar <chippenitishkumar@gmail.com>
2023-10-24 05:48:17 +0000

    IDE errors Failed to Load

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import UserProfile from './components/UserProfile/index'
+userDetails = {
+  imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+  name: 'Rahul',
+  role: 'Software Developer',
+}
+const App = () => <UserProfile />
+
+export default App
---
+++ src/components/UserProfile/index.js
@@ -0,0 +1,5 @@
+const UserProfile = () => (
+  <img src ="https://assets.ccbp.in/frontend/react-js/devon-lane-img.png" />
+)
+
+export default UserProfile
